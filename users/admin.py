4eb337fae23ea932da73080a1fe547d9caa9107b
Matvey <matvey.agarkov02@gmail.com>
2022-11-17 23:35:35 +0300

    one more commit

--- users/admin.py
+++ users/admin.py
@@ -4,11 +4,11 @@ from users.models import User
 
 
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('nickname', 'reg_date', 'is_online')
+    list_display = ('username', 'reg_date', 'is_online')
     list_filter = ('is_online',)
     raw_id_fields = ('chats',)
     list_editable = ('is_online',)
-    search_fields = ('nickname',)
+    search_fields = ('username',)
 
 
 admin.site.register(User, UserAdmin)
