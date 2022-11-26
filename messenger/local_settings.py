8086fcf51e0655dc8888c5f15394c574548fc53e
m_agarkov <96927525+Cpr4z@users.noreply.github.com>
2022-11-26 15:17:47 +0300

    Create local_settings.py

---
+++ messenger/local_settings.py
@@ -0,0 +1,7 @@
+SECRET_KEY = 'django-insecure-es!5ask-3kd&vlje_9-hw!jxv=go@lt2xh)*&)-@j=^^jr8fi3'
+DEBUG = True
+ALLOWED_HOSTS = ['127.0.0.1',
+                 'localhost']
+DB_NAME = 'messenger_db'
+DB_USER = 'matvey'
+DB_PASSWORD = '123'
